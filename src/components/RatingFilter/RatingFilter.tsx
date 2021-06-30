ed6386675ebc2101d5f8b081d3271f4eaa39c37b
Alex Nosse <a.k.nosse@gmail.com>
2021-06-29 17:49:43 -0700

    Refactoring the filter component to be activated by a single click

--- src/components/RatingFilter/RatingFilter.tsx
+++ src/components/RatingFilter/RatingFilter.tsx
@@ -10,58 +10,34 @@ import { faStar as faStarLight } from '@fortawesome/free-regular-svg-icons';
 import Style from './RatingFilter.style';
 import { useAppDispatch } from '../../app/hooks';
 import { ratingChanged } from '../../features/movies/moviesSlice';
+
 const possibleRatings = [2, 4, 6, 8, 10];
 export function RatingFilter() {
   const dispatch = useAppDispatch();
 
-  const [isOpen, setOpen] = useState(false);
   const [rating, setRating] = useState(0);
-  const [tempRating, setTempRating] = useState(0);
-
-  const onOpen = useCallback(() => {
-    setOpen(!isOpen);
-  }, [isOpen]);
 
   const setRatingOnClick = useCallback(
     (newRating: number) => {
-      setTempRating(newRating === tempRating ? 0 : newRating);
+      const updatedRating = newRating === rating ? 0 : newRating;
+      setRating(updatedRating);
+      dispatch(ratingChanged({ rating: updatedRating }));
     },
-    [tempRating],
+    [rating],
   );
 
-  const onConfirmFilter = useCallback(() => {
-    setRating(tempRating);
-    dispatch(ratingChanged({ rating: tempRating }));
-    setOpen(false);
-  }, [tempRating, dispatch]);
-
-  const onCancelFilter = useCallback(() => {
-    setTempRating(rating);
-    setOpen(false);
-  }, [rating]);
-
   return (
-    <>
-      <Style.RatingContainer onClick={() => (!isOpen ? onOpen() : undefined)}>
-        <p>Filter</p>
-        <FontAwesomeIcon icon={!isOpen ? faCaretDown : faCaretUp} />
-        <Style.StarsContainer visible={isOpen}>
-          {possibleRatings.map((possibleRating) => (
-            <FontAwesomeIcon
-              icon={tempRating >= possibleRating ? faStar : faStarLight}
-              onClick={() => setRatingOnClick(possibleRating)}
-              key={possibleRating}
-            />
-          ))}
-          <Style.ApplyFilterButton onClick={onConfirmFilter} visible={isOpen}>
-            Apply filter
-          </Style.ApplyFilterButton>
-        </Style.StarsContainer>
-      </Style.RatingContainer>
-      <Style.RatingBackdrop
-        visible={isOpen}
-        onClick={onCancelFilter}
-      ></Style.RatingBackdrop>
-    </>
+    <Style.RatingContainer>
+      <p>Filter by Rating:</p>
+      <Style.StarsContainer>
+        {possibleRatings.map((possibleRating) => (
+          <FontAwesomeIcon
+            icon={rating >= possibleRating ? faStar : faStarLight}
+            onClick={() => setRatingOnClick(possibleRating)}
+            key={possibleRating}
+          />
+        ))}
+      </Style.StarsContainer>
+    </Style.RatingContainer>
   );
 }
